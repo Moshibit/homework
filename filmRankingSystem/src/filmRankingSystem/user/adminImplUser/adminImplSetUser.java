ad40f382ce90666fb50bdd54fe39580647f0e9b5
Moshibit <39635527+Moshibit@users.noreply.github.com>
2018-07-19 17:17:54 -0500

    Add files via upload

---
+++ filmRankingSystem/src/filmRankingSystem/user/adminImplUser/adminImplSetUser.java
@@ -0,0 +1,22 @@
+/**
+ * create: 19/07/2018
+ */
+package filmRankingSystem.user.adminImplUser;
+
+import filmRankingSystem.user.adminUser.adminUser;
+
+/**
+ * @author Erik J
+ */
+public class adminImplSetUser implements adminUser
+{
+
+    /**
+     * 
+     */
+    public adminImplSetUser()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+}
